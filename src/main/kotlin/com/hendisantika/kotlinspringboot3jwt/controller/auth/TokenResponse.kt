dd931c48f60da986f3e8da44277b112204419b02
hendisantika <hendisantika@yahoo.co.id>
2023-10-31 15:53:59 +0700

    #57 - Add TokenResponse in AuthController ✏️📗:octocat:🐧

---
+++ src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/controller/auth/TokenResponse.kt
@@ -0,0 +1,15 @@
+package com.hendisantika.kotlinspringboot3jwt.controller.auth
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-spring-boot-3-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/31/23
+ * Time: 15:53
+ * To change this template use File | Settings | File Templates.
+ */
+data class TokenResponse(
+    val token: String
+)

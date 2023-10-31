81b6b7f9c30c1be9e6de8e3a5c8f1b3272adecd4
hendisantika <hendisantika@yahoo.co.id>
2023-10-31 15:29:08 +0700

    #30 - Add CustomUserDetailsService ✏️📗:octocat:🐧

---
+++ src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/service/CustomUserDetailsService.kt
@@ -0,0 +1,20 @@
+package com.hendisantika.kotlinspringboot3jwt.service
+
+import com.hendisantika.kotlinspringboot3jwt.repository.UserRepository
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-spring-boot-3-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/31/23
+ * Time: 15:28
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+class CustomUserDetailsService(
+    private val userRepository: UserRepository
+) : UserDetailsService

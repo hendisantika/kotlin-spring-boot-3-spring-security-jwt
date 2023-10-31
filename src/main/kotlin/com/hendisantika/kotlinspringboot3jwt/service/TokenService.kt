1120471a7e78cd4b7e4ec25c297dbd34ee6714d0
hendisantika <hendisantika@yahoo.co.id>
2023-10-31 15:33:01 +0700

    #34 - Add generate in TokenService ✏️📗:octocat:🐧

--- src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/service/TokenService.kt
+++ src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/service/TokenService.kt
@@ -1,7 +1,11 @@
 package com.hendisantika.kotlinspringboot3jwt.service
 
 import com.hendisantika.kotlinspringboot3jwt.config.JwtProperties
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Service
+import java.util.*
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +20,23 @@ import org.springframework.stereotype.Service
 @Service
 class TokenService(
     jwtProperties: JwtProperties
-)
+) {
+    private val secretKey = Keys.hmacShaKeyFor(
+        jwtProperties.key.toByteArray()
+    )
+
+    fun generate(
+        userDetails: UserDetails,
+        expirationDate: Date,
+        additionalClaims: Map<String, Any> = emptyMap()
+    ): String =
+        Jwts.builder()
+            .claims()
+            .subject(userDetails.username)
+            .issuedAt(Date(System.currentTimeMillis()))
+            .expiration(expirationDate)
+            .add(additionalClaims)
+            .and()
+            .signWith(secretKey)
+            .compact()
+}

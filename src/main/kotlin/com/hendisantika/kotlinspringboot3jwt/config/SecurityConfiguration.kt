f9b63357103a4f2311e66d289e6bbbd61e71d58d
hendisantika <hendisantika@yahoo.co.id>
2023-10-31 15:49:20 +0700

    #50 - Add securityFilterChain in SecurityConfiguration ✏️📗:octocat:🐧

--- src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/config/SecurityConfiguration.kt
@@ -1,8 +1,14 @@
 package com.hendisantika.kotlinspringboot3jwt.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +24,31 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 class SecurityConfiguration(
     private val authenticationProvider: AuthenticationProvider
-)
+) {
+    @Bean
+    fun securityFilterChain(
+        http: HttpSecurity,
+        jwtAuthenticationFilter: JwtAuthenticationFilter
+    ): DefaultSecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers("/api/auth", "api/auth/refresh", "/error")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/user")
+                    .permitAll()
+                    .requestMatchers("/api/user**")
+                    .hasRole("ADMIN")
+                    .anyRequest()
+                    .fullyAuthenticated()
+            }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+    }
+}

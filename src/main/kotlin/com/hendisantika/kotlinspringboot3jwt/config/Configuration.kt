e6703406b492519022ca17c435c990e8976fb9a4
hendisantika <hendisantika@yahoo.co.id>
2023-10-31 15:11:04 +0700

    #4 - Add Configuration.kt ✏️📗:octocat:🐧

---
+++ src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/config/Configuration.kt
@@ -0,0 +1,19 @@
+package com.hendisantika.kotlinspringboot3jwt.config
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-spring-boot-3-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/31/23
+ * Time: 15:10
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableConfigurationProperties(JwtProperties::class) {
+
+}

f51ca41cf2d6f573dc9619d18271682517d2edc2
hendisantika <hendisantika@yahoo.co.id>
2023-10-31 15:54:51 +0700

    #59 - Add AuthenticationResponse in AuthController ✏️📗:octocat:🐧

---
+++ src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/controller/auth/AuthenticationResponse.kt
@@ -0,0 +1,16 @@
+package com.hendisantika.kotlinspringboot3jwt.controller.auth
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-spring-boot-3-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/31/23
+ * Time: 15:54
+ * To change this template use File | Settings | File Templates.
+ */
+data class AuthenticationResponse(
+    val accessToken: String,
+    val refreshToken: String,
+)

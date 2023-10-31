d1a48b1acdc6a24e29bfea24c4a3a230adde0122
hendisantika <hendisantika@yahoo.co.id>
2023-10-31 12:22:28 +0700

    #1 - Init Starter Project ✏️📗:octocat:🐧

---
+++ src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/KotlinSpringBoot3JwtApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.kotlinspringboot3jwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBoot3JwtApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBoot3JwtApplication>(*args)
+}
---
+++ src/test/kotlin/com/hendisantika/kotlinspringboot3jwt/KotlinSpringBoot3JwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hendisantika.kotlinspringboot3jwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBoot3JwtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

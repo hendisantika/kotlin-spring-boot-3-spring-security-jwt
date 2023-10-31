2aa1c3641ddfe9dfabb0fe01e53a345caacb614b
hendisantika <hendisantika@yahoo.co.id>
2023-10-31 15:32:18 +0700

    #33 - Add TokenService ✏️📗:octocat:🐧

---
+++ src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/service/TokenService.kt
@@ -0,0 +1,19 @@
+package com.hendisantika.kotlinspringboot3jwt.service
+
+import com.hendisantika.kotlinspringboot3jwt.config.JwtProperties
+import org.springframework.stereotype.Service
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-spring-boot-3-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/31/23
+ * Time: 15:31
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+class TokenService(
+    jwtProperties: JwtProperties
+)

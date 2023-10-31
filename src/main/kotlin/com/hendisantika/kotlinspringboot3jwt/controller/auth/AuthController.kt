224abd48c29a76ab72e1b9a2f960492c585ca4f3
hendisantika <hendisantika@yahoo.co.id>
2023-10-31 15:53:36 +0700

    #56 - Add authenticate in AuthController ✏️📗:octocat:🐧

--- src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/controller/auth/AuthController.kt
+++ src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/controller/auth/AuthController.kt
@@ -1,6 +1,8 @@
 package com.hendisantika.kotlinspringboot3jwt.controller.auth
 
 import com.hendisantika.kotlinspringboot3jwt.service.AuthenticationService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -18,4 +20,10 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/auth")
 class AuthController(
     private val authenticationService: AuthenticationService
-)
+) {
+    @PostMapping
+    fun authenticate(
+        @RequestBody authRequest: AuthenticationRequest
+    ): AuthenticationResponse =
+        authenticationService.authentication(authRequest)
+}

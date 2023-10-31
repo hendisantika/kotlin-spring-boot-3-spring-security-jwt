adf79e191dc0bd5eb69b3e56efe2b49f4a24a5af
hendisantika <hendisantika@yahoo.co.id>
2023-10-31 15:24:54 +0700

    #25 - Add refreshAccessToken in AuthenticationService ✏️📗:octocat:🐧

--- src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/service/AuthenticationService.kt
+++ src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/service/AuthenticationService.kt
@@ -23,8 +23,9 @@ class AuthenticationService(
     private val userDetailsService: CustomUserDetailsService,
     private val tokenService: TokenService,
     private val jwtProperties: JwtProperties,
-    private val refreshTokenRepository: RefreshTokenRepository,
+    private val refreshTokenRepository: RefreshTokenRepository
 ) {
+
     fun authentication(authenticationRequest: AuthenticationRequest): AuthenticationResponse {
         authManager.authenticate(
             UsernamePasswordAuthenticationToken(
@@ -33,3 +34,31 @@ class AuthenticationService(
             )
         )
     }
+
+    val user = userDetailsService.loadUserByUsername(authenticationRequest.email)
+
+    val accessToken = createAccessToken(user)
+    val refreshToken = createRefreshToken(user)
+
+    refreshTokenRepository.save(refreshToken, user)
+
+    return AuthenticationResponse(
+    accessToken = accessToken,
+    refreshToken = refreshToken
+    )
+
+    fun refreshAccessToken(refreshToken: String): String? {
+        val extractedEmail = tokenService.extractEmail(refreshToken)
+
+        return extractedEmail?.let { email ->
+            val currentUserDetails = userDetailsService.loadUserByUsername(email)
+            val refreshTokenUserDetails = refreshTokenRepository.findUserDetailsByToken(refreshToken)
+
+            if (!tokenService.isExpired(refreshToken) && refreshTokenUserDetails?.username == currentUserDetails.username)
+                createAccessToken(currentUserDetails)
+            else
+                null
+        }
+    }
+}
+

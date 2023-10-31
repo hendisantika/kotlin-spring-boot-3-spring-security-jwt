47dcbe3e4723fcc7b6201d91a2ce667def918c8b
hendisantika <hendisantika@yahoo.co.id>
2023-10-31 15:21:39 +0700

    #21 - Add ArticleService ✏️📗:octocat:🐧

---
+++ src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/service/ArticleService.kt
@@ -0,0 +1,17 @@
+package com.hendisantika.kotlinspringboot3jwt.service
+
+import com.hendisantika.kotlinspringboot3jwt.repository.ArticleRepository
+import org.springframework.stereotype.Service
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-spring-boot-3-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/31/23
+ * Time: 15:20
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+class ArticleService(private val articleRepository: ArticleRepository)

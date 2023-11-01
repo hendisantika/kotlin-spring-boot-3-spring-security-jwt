50601f7fe182fb813ea99e2cf6b7a5fed3a13192
hendisantika <hendisantika@yahoo.co.id>
2023-11-01 11:24:27 +0700

    #64 - Add create in UserController ✏️📗:octocat:⬆

--- src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/controller/user/UserController.kt
+++ src/main/kotlin/com/hendisantika/kotlinspringboot3jwt/controller/user/UserController.kt
@@ -1,8 +1,12 @@
 package com.hendisantika.kotlinspringboot3jwt.controller.user
 
 import com.hendisantika.kotlinspringboot3jwt.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +22,12 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/user")
 class UserController(
     private val userService: UserService
-)
+) {
+
+    @PostMapping
+    fun create(@RequestBody userRequest: UserRequest): UserResponse =
+        userService.createUser(userRequest.toModel())
+            ?.toResponse()
+            ?: throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Cannot create user.")
+
+}

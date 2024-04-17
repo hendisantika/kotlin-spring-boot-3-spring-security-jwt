189f3c3f77efbea1c609ec4e13d38a545a88c305
hendisantika <hendisantika@yahoo.co.id>
2024-04-17 13:46:16 +0700

    #79 - Upgraded into Spring Boot 3.2.1 version🫸🌀✏️📗🐧🐳

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.0"
+    id("org.springframework.boot") version "3.2.1"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
@@ -11,7 +11,7 @@ group = "com.hendisantika"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
